9667868870ee085f42d75ffcd2a1f662c9c6704e
Ananda.Silva-ext <Ananda.Silva-ext@ab-Inbev.com>
2022-09-01 11:17:35 -0300

    adding prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // ponto e virgula no final das linhas?
+  semi: true,
+  // aspas simples?
+  singleQuote: false,
+  // arrow functions que tem unico paramentro coloca parenteses?
+  arrowParens: "avoid",
+  // mantem a virgula no final?
+  trailingComma: "none",
+  // LF, CRLF
+  endOfLine: "auto",
+};
